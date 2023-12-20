f472a394547eadb534a274ff3ad9acfd66393aeb
zjun <565061763@qq.com>
2023-12-20 12:38:44 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_input/SampleTextField.kt
+++ app/src/main/java/com/sd/demo/compose_input/SampleTextField.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,10 +39,11 @@ class SampleTextField : ComponentActivity() {
 private fun Content() {
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
     ) {
         Sample(
             modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant),
