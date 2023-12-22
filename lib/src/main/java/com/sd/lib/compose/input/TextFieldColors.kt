677127c618251b93cdc5aaaf33ecbfc7285dda2d
zjun <565061763@qq.com>
2023-12-22 21:31:37 +0800

    refactor

--- lib/src/main/java/com/sd/lib/compose/input/TextFieldColors.kt
+++ lib/src/main/java/com/sd/lib/compose/input/TextFieldColors.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.sd.lib.compose.input
 
 import androidx.compose.animation.animateColorAsState
@@ -61,107 +45,107 @@ data class FTextFieldColors internal constructor(
     val unfocusedTrailingIconColor: Color,
     val disabledTrailingIconColor: Color,
     val errorTrailingIconColor: Color,
-) {
-    @Composable
-    internal fun textColor(
-        enabled: Boolean,
-        isError: Boolean,
-        focused: Boolean,
-    ): State<Color> {
-        val targetValue = when {
-            !enabled -> disabledTextColor
-            isError -> errorTextColor
-            focused -> focusedTextColor
-            else -> unfocusedTextColor
+)
+
+@Composable
+internal fun FTextFieldColors.textColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): State<Color> {
+    val targetValue = when {
+        !enabled -> disabledTextColor
+        isError -> errorTextColor
+        focused -> focusedTextColor
+        else -> unfocusedTextColor
+    }
+    return rememberUpdatedState(targetValue)
+}
+
+@Composable
+internal fun FTextFieldColors.containerColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): State<Color> {
+    val targetValue = when {
+        !enabled -> disabledContainerColor
+        isError -> errorContainerColor
+        focused -> focusedContainerColor
+        else -> unfocusedContainerColor
+    }
+    return animateColorAsState(targetValue, tween(durationMillis = AnimationDuration), label = "")
+}
+
+@Composable
+internal fun FTextFieldColors.cursorColor(isError: Boolean): State<Color> {
+    return rememberUpdatedState(if (isError) errorCursorColor else cursorColor)
+}
+
+@Composable
+internal fun FTextFieldColors.indicatorColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): State<Color> {
+    val targetValue = when {
+        !enabled -> disabledIndicatorColor
+        isError -> errorIndicatorColor
+        focused -> focusedIndicatorColor
+        else -> unfocusedIndicatorColor
+    }
+    return if (enabled) {
+        animateColorAsState(targetValue, tween(durationMillis = AnimationDuration), label = "")
+    } else {
+        rememberUpdatedState(targetValue)
+    }
+}
+
+@Composable
+internal fun FTextFieldColors.placeholderColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): State<Color> {
+    val targetValue = when {
+        !enabled -> disabledPlaceholderColor
+        isError -> errorPlaceholderColor
+        focused -> focusedPlaceholderColor
+        else -> unfocusedPlaceholderColor
+    }
+    return rememberUpdatedState(targetValue)
+}
+
+@Composable
+internal fun FTextFieldColors.leadingIconColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): State<Color> {
+    return rememberUpdatedState(
+        when {
+            !enabled -> disabledLeadingIconColor
+            isError -> errorLeadingIconColor
+            focused -> focusedLeadingIconColor
+            else -> unfocusedLeadingIconColor
         }
-        return rememberUpdatedState(targetValue)
-    }
+    )
+}
 
-    @Composable
-    internal fun containerColor(
-        enabled: Boolean,
-        isError: Boolean,
-        focused: Boolean,
-    ): State<Color> {
-        val targetValue = when {
-            !enabled -> disabledContainerColor
-            isError -> errorContainerColor
-            focused -> focusedContainerColor
-            else -> unfocusedContainerColor
+@Composable
+internal fun FTextFieldColors.trailingIconColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): State<Color> {
+    return rememberUpdatedState(
+        when {
+            !enabled -> disabledTrailingIconColor
+            isError -> errorTrailingIconColor
+            focused -> focusedTrailingIconColor
+            else -> unfocusedTrailingIconColor
         }
-        return animateColorAsState(targetValue, tween(durationMillis = AnimationDuration), label = "")
-    }
-
-    @Composable
-    internal fun cursorColor(isError: Boolean): State<Color> {
-        return rememberUpdatedState(if (isError) errorCursorColor else cursorColor)
-    }
-
-    @Composable
-    internal fun indicatorColor(
-        enabled: Boolean,
-        isError: Boolean,
-        focused: Boolean,
-    ): State<Color> {
-        val targetValue = when {
-            !enabled -> disabledIndicatorColor
-            isError -> errorIndicatorColor
-            focused -> focusedIndicatorColor
-            else -> unfocusedIndicatorColor
-        }
-        return if (enabled) {
-            animateColorAsState(targetValue, tween(durationMillis = AnimationDuration), label = "")
-        } else {
-            rememberUpdatedState(targetValue)
-        }
-    }
-
-    @Composable
-    internal fun placeholderColor(
-        enabled: Boolean,
-        isError: Boolean,
-        focused: Boolean,
-    ): State<Color> {
-        val targetValue = when {
-            !enabled -> disabledPlaceholderColor
-            isError -> errorPlaceholderColor
-            focused -> focusedPlaceholderColor
-            else -> unfocusedPlaceholderColor
-        }
-        return rememberUpdatedState(targetValue)
-    }
-
-    @Composable
-    internal fun leadingIconColor(
-        enabled: Boolean,
-        isError: Boolean,
-        focused: Boolean,
-    ): State<Color> {
-        return rememberUpdatedState(
-            when {
-                !enabled -> disabledLeadingIconColor
-                isError -> errorLeadingIconColor
-                focused -> focusedLeadingIconColor
-                else -> unfocusedLeadingIconColor
-            }
-        )
-    }
-
-    @Composable
-    internal fun trailingIconColor(
-        enabled: Boolean,
-        isError: Boolean,
-        focused: Boolean,
-    ): State<Color> {
-        return rememberUpdatedState(
-            when {
-                !enabled -> disabledTrailingIconColor
-                isError -> errorTrailingIconColor
-                focused -> focusedTrailingIconColor
-                else -> unfocusedTrailingIconColor
-            }
-        )
-    }
+    )
 }
 
 private const val AnimationDuration = 150
