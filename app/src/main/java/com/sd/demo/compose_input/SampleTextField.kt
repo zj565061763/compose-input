0ea28763665c7bade3c4218904078740026ff9ce
zjun <565061763@qq.com>
2023-12-22 21:29:41 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_input/SampleTextField.kt
+++ app/src/main/java/com/sd/demo/compose_input/SampleTextField.kt
@@ -3,10 +3,14 @@ package com.sd.demo.compose_input
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +23,8 @@ import androidx.compose.ui.unit.dp
 import com.sd.demo.compose_input.ui.theme.AppTheme
 import com.sd.lib.compose.input.FTextField
 import com.sd.lib.compose.input.FTextFieldIconClear
+import com.sd.lib.compose.input.fTextFieldState
+import com.sd.lib.compose.input.indicatorColor
 
 class SampleTextField : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,13 +51,34 @@ private fun Content() {
         FTextField(
             value = value,
             maxLines = 2,
+            shape = RoundedCornerShape(10.dp),
             placeholder = { Text(text = "placeholder") },
-            trailingIcon = { FTextFieldIconClear() },
             onFocusRequester = { it.requestFocus() },
         ) {
             value = it
         }
 
-        FTextField(value = "value") {}
+        SampleIndicator()
+    }
+}
+
+@Composable
+private fun SampleIndicator(
+    modifier: Modifier = Modifier,
+) {
+    var value by remember { mutableStateOf("") }
+    FTextField(
+        modifier = modifier,
+        value = value,
+        trailingIcon = { FTextFieldIconClear(modifier = Modifier.padding(end = 10.dp)) },
+        indicator = {
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .border(1.dp, fTextFieldState().indicatorColor().value, CircleShape)
+            )
+        },
+    ) {
+        value = it
     }
 }
