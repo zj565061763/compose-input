0e4a84cd1d8b7592ccde1ed653090602a654f977
zjun <565061763@qq.com>
2024-11-16 23:58:49 +0800

    placeholder

--- lib/src/main/java/com/sd/lib/compose/input/TextFieldDecoration.kt
+++ lib/src/main/java/com/sd/lib/compose/input/TextFieldDecoration.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
@@ -59,10 +60,13 @@ internal fun DecorationBox(
             contentAlignment = Alignment.Center,
          ) {
             placeholder?.let {
-               if (state.text.isEmpty()) {
-                  Decoration(
-                     contentColor = state.placeholderColor(),
-                     textStyle = textStyle,
+               Decoration(
+                  contentColor = state.placeholderColor(),
+                  textStyle = textStyle,
+               ) {
+                  Box(
+                     modifier = Modifier.alpha(if (state.text.isEmpty()) 1f else 0f),
+                     propagateMinConstraints = true,
                   ) {
                      it()
                   }
