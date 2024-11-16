fc49be71bb61b3f8295b024c40775970a4039c2d
zjun <565061763@qq.com>
2024-11-17 00:32:17 +0800

    1.3.0-alpha02

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-input"
-val libVersion = "1.3.0-alpha01"
+val libVersion = "1.3.0-alpha02"
 
 android {
    namespace = "com.sd.lib.compose.input"
