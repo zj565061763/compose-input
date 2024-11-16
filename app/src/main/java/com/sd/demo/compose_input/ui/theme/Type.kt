c25c67d8a3e3a05e598417159d086ab3baa3dad5
zjun <565061763@qq.com>
2024-11-17 04:09:15 +0800

    revert

--- app/src/main/java/com/sd/demo/compose_input/ui/theme/Type.kt
+++ app/src/main/java/com/sd/demo/compose_input/ui/theme/Type.kt
@@ -1,79 +1,34 @@
 package com.sd.demo.compose_input.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
-val Typography = run {
-   val default = Typography()
-   val defaultLineHeight = (1.0).em
-   val defaultLetterSpacing = (0.1).sp
-   Typography(
-      displayLarge = default.displayLarge.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      displayMedium = default.displayMedium.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      displaySmall = default.displaySmall.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-
-      headlineLarge = default.headlineLarge.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      headlineMedium = default.headlineMedium.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      headlineSmall = default.headlineSmall.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-
-      titleLarge = default.titleLarge.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      titleMedium = default.titleMedium.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      titleSmall = default.titleSmall.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-
-      bodyLarge = default.bodyLarge.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      bodyMedium = default.bodyMedium.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      bodySmall = default.bodySmall.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-
-      labelLarge = default.labelLarge.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-         fontWeight = FontWeight.Medium,
-      ),
-      labelMedium = default.labelMedium.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
-      labelSmall = default.labelSmall.copy(
-         lineHeight = defaultLineHeight,
-         letterSpacing = defaultLetterSpacing,
-      ),
+// Set of Material typography styles to start with
+val Typography = Typography(
+   bodyLarge = TextStyle(
+      fontFamily = FontFamily.Default,
+      fontWeight = FontWeight.Normal,
+      fontSize = 16.sp,
+      lineHeight = 24.sp,
+      letterSpacing = 0.5.sp
    )
-}
+   /* Other default text styles to override
+   titleLarge = TextStyle(
+       fontFamily = FontFamily.Default,
+       fontWeight = FontWeight.Normal,
+       fontSize = 22.sp,
+       lineHeight = 28.sp,
+       letterSpacing = 0.sp
+   ),
+   labelSmall = TextStyle(
+       fontFamily = FontFamily.Default,
+       fontWeight = FontWeight.Medium,
+       fontSize = 11.sp,
+       lineHeight = 16.sp,
+       letterSpacing = 0.5.sp
+   )
+   */
+)
