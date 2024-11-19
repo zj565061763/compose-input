bbbfb8052b935c19b0c67c699bdb751cf6eba448
zjun <565061763@qq.com>
2024-11-19 21:59:22 +0800

    ext

--- lib/src/main/java/com/sd/lib/compose/input/TextField.kt
+++ lib/src/main/java/com/sd/lib/compose/input/TextField.kt
@@ -1,18 +1,11 @@
 package com.sd.lib.compose.input
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -22,18 +15,12 @@ import androidx.compose.foundation.text.input.OutputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.takeOrElse
@@ -42,7 +29,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -135,97 +121,5 @@ fun FTextField(
             }
          }
       )
-   }
-}
-
-//-------------------- Ext --------------------
-
-/**
- * 指示器边框
- */
-@Composable
-fun BoxScope.FTextFieldIndicator(
-   modifier: Modifier = Modifier,
-   shape: Shape = MaterialTheme.shapes.extraSmall,
-   unfocusedThickness: Dp = 1.dp,
-   focusedThickness: Dp = unfocusedThickness * 1.2f,
-) {
-   val state = fTextFieldState()
-   val thicknessAnim = animateDpAsState(
-      targetValue = if (state.focused) focusedThickness else unfocusedThickness,
-      label = "TextField indicator thickness"
-   )
-   Box(
-      modifier = modifier
-         .matchParentSize()
-         .border(
-            width = thicknessAnim.value,
-            color = state.indicatorColor(),
-            shape = shape,
-         )
-   )
-}
-
-/**
- * 清空内容
- */
-@Composable
-fun FTextFieldIconClear(
-   modifier: Modifier = Modifier,
-   shape: Shape = CircleShape,
-   containerColor: Color = Color.Transparent,
-   icon: @Composable () -> Unit = {
-      Icon(
-         modifier = Modifier.size(16.dp),
-         imageVector = Icons.Default.Clear,
-         contentDescription = "clear",
-      )
-   },
-) {
-   val state = fTextFieldState()
-   val showIcon = state.focused && state.text.isNotEmpty()
-
-   FTextFieldIcon(
-      modifier = modifier,
-      shape = shape,
-      containerColor = containerColor,
-      onClick = if (showIcon) {
-         {
-            state.clearText()
-         }
-      } else null,
-   ) {
-      if (showIcon) {
-         icon()
-      }
-   }
-}
-
-/**
- * 图标容器
- */
-@Composable
-fun FTextFieldIcon(
-   modifier: Modifier = Modifier,
-   shape: Shape = CircleShape,
-   containerColor: Color = Color.Transparent,
-   onClick: (() -> Unit)? = null,
-   icon: @Composable () -> Unit,
-) {
-   Box(
-      modifier = modifier
-         .defaultMinSize(24.dp, 24.dp)
-         .background(color = containerColor, shape = shape)
-         .clip(shape)
-         .let {
-            if (onClick != null) {
-               it.clickable { onClick() }
-            } else {
-               it
-            }
-         },
-      contentAlignment = Alignment.Center,
-   ) {
-      icon()
    }
 }
--- lib/src/main/java/com/sd/lib/compose/input/TextFieldDefaults.kt
+++ lib/src/main/java/com/sd/lib/compose/input/TextFieldDefaults.kt
@@ -1,9 +1,27 @@
 package com.sd.lib.compose.input
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 object FTextFieldDefaults {
    @Suppress("NAME_SHADOWING")
@@ -106,5 +124,97 @@ object FTextFieldDefaults {
             backgroundColor = cursorColor.copy(alpha = 0.4f)
          )
       )
+   }
+}
+
+//-------------------- Ext --------------------
+
+/**
+ * 指示器边框
+ */
+@Composable
+fun BoxScope.FTextFieldIndicator(
+   modifier: Modifier = Modifier,
+   shape: Shape = MaterialTheme.shapes.extraSmall,
+   unfocusedThickness: Dp = 1.dp,
+   focusedThickness: Dp = unfocusedThickness * 1.2f,
+) {
+   val state = fTextFieldState()
+   val thicknessAnim = animateDpAsState(
+      targetValue = if (state.focused) focusedThickness else unfocusedThickness,
+      label = "TextField indicator thickness"
+   )
+   Box(
+      modifier = modifier
+         .matchParentSize()
+         .border(
+            width = thicknessAnim.value,
+            color = state.indicatorColor(),
+            shape = shape,
+         )
+   )
+}
+
+/**
+ * 清空内容
+ */
+@Composable
+fun FTextFieldIconClear(
+   modifier: Modifier = Modifier,
+   shape: Shape = CircleShape,
+   containerColor: Color = Color.Transparent,
+   icon: @Composable () -> Unit = {
+      Icon(
+         modifier = Modifier.size(16.dp),
+         imageVector = Icons.Default.Clear,
+         contentDescription = "clear",
+      )
+   },
+) {
+   val state = fTextFieldState()
+   val showIcon = state.focused && state.text.isNotEmpty()
+
+   FTextFieldIcon(
+      modifier = modifier,
+      shape = shape,
+      containerColor = containerColor,
+      onClick = if (showIcon) {
+         {
+            state.clearText()
+         }
+      } else null,
+   ) {
+      if (showIcon) {
+         icon()
+      }
+   }
+}
+
+/**
+ * 图标容器
+ */
+@Composable
+fun FTextFieldIcon(
+   modifier: Modifier = Modifier,
+   shape: Shape = CircleShape,
+   containerColor: Color = Color.Transparent,
+   onClick: (() -> Unit)? = null,
+   icon: @Composable () -> Unit,
+) {
+   Box(
+      modifier = modifier
+         .defaultMinSize(24.dp, 24.dp)
+         .background(color = containerColor, shape = shape)
+         .clip(shape)
+         .let {
+            if (onClick != null) {
+               it.clickable { onClick() }
+            } else {
+               it
+            }
+         },
+      contentAlignment = Alignment.Center,
+   ) {
+      icon()
    }
 }
